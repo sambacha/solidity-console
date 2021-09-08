ad7240485d7cb8ff8ed29c564a54e94a417fcfb8
sam bacha <sam@manifoldfinance.com>
2021-09-07 19:05:26 -0700

    feat(init): v0.0.1
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

---
+++ index.ts
@@ -0,0 +1,13 @@
+
+/**
+* @package solidity console 
+* @version 0.0.1
+* @summary hardhat console, hevm
+*/
+
+import { default as hardhat-console } from "./hardhat-console";
+import { default as hevm} from "./hemv";
+export const solidity-console = {
+ hardhat-console,
+ hevm,
+};
